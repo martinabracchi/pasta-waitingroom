4d9bfb7fdfa4cc71684df0b81866e4023992d2b9
martinabracchi <71330038+martinabracchi@users.noreply.github.com>
2021-01-19 16:10:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 console.log("up and running");
 let express = require('express');
+let socket = require('socket.io')
 let app = express();
 let port = 3000;
 let server = app.listen(port);
@@ -8,5 +9,5 @@ let io = socket(server);
 
 io.on('connection', newConnection);
 function newConnection(socket){
-	console.log(new connection:', socket.client.id);
+	console.log('new connection'+ socket.client.id);
 }
